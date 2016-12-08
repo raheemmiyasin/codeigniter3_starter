bc8af02637887a2c9b5eee571e501c338a1ca5e2
raheemmi <raheemmi.yasin@corous360.com>
2016-12-08 18:26:17 +0800

    minor changes

--- application/views/_layouts/header.php
+++ application/views/_layouts/header.php
@@ -52,13 +52,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?php echo base_url('assets/alte/img/user2-160x160.jpg'); ?>" class="user-image" alt="User Image">
+              <img src="<?php echo base_url('assets/alte/img/avatar5.png'); ?>" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="<?php echo base_url('assets/alte/img/user2-160x160.jpg'); ?>" class="img-circle" alt="User Image">
+                <img src="<?php echo base_url('assets/alte/img/avatar5.png'); ?>" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
@@ -105,10 +105,10 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?php echo base_url('assets/alte/img/user2-160x160.jpg'); ?>" class="img-circle" alt="User Image">
+          <img src="<?php echo base_url('assets/alte/img/avatar5.png'); ?>" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Ahmad Albab</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
